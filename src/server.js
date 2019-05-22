76d0d8ea6e826fe25bd773ae6d16121e0d39bc64
flnutmmyungju <mjmj01617@naver.com>
2019-05-22 10:07:56 +0900

    pug_set

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
+import { join } from "path";
 import express from "express";
+
+const PORT = 4000;
+const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.get("/", (rec, res) => res.render("home"));
+
+const handleListening = () =>
+  console.log(`v Server running: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
